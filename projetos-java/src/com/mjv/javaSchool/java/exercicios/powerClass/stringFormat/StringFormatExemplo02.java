7da739604310915dd9f054a4c64925f08295b130
Victor Henrique <vhsdm7@gmail.com>
2022-04-20 18:58:24 -0300

    Uptade ex2 StringFormat

---
+++ projetos-java/src/com/mjv/javaSchool/java/exercicios/powerClass/stringFormat/StringFormatExemplo02.java
@@ -0,0 +1,10 @@
+package com.mjv.javaSchool.java.exercicios.powerClass.stringFormat;
+
+public class StringFormatExemplo02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
