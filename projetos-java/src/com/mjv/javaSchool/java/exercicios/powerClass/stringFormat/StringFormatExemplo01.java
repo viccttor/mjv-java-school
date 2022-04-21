52d06658e8d2d4ff2f43af69b50664ab23420d9a
Victor Henrique <vhsdm7@gmail.com>
2022-04-21 01:27:25 -0300

    Adcionado mais exemplos

--- projetos-java/src/com/mjv/javaSchool/java/exercicios/powerClass/stringFormat/StringFormatExemplo01.java
+++ projetos-java/src/com/mjv/javaSchool/java/exercicios/powerClass/stringFormat/StringFormatExemplo01.java
@@ -14,12 +14,35 @@ public class StringFormatExemplo01 {
 		System.out.println("Nome: " + nome + "\tSobrenome: " + sobrenome + "\tIdade: " + idade + "\tSalário: " + salario);
 		
 		// Utilizando o recurso printf para pode colocar na setença a variável.
-		System.out.printf("\n\nNome %s %s, tem %d anos e ganha R$%f por mês.", nome,sobrenome,idade,salario);
+		String teste1 = String.format("\n\nOutra Forma01: Nome: %s %s, tem %d anos e ganha R$%3f por mês.", nome,sobrenome,idade,salario);
+		System.out.println(teste1);
 		
 		// Nesta condição, utilizamos um recurso para controlar o número de casas decimais do tipo float ou double
-		System.out.printf("\n\nOutra Forma: Nome: %s %s, tem %d anos e ganha R$%.3f por mês.", nome,sobrenome,idade,salario);
-
-
+		System.out.printf("\n\nOutra Forma02: Nome %s %s, tem %d anos e ganha R$%.3f por mês.\n", nome,sobrenome,idade,salario);
+		
+		// Utilizando o %-20d, ele especifica a largura apos
+		System.out.println();
+		String teste2 = String.format("|%-20d| teste2", 93);
+		System.out.println(teste2);
+		
+		// Utilizando o %20d, ele especifica a largura antes
+		System.out.println();
+		String teste3 = String.format("|%20d| teste2", 93);
+		System.out.println(teste3);
+		
+		// Utilizando o %020d, bloco com zeros
+		System.out.println();
+		String teste4 = String.format("|%020d| teste2", 93);
+		System.out.println(teste4);
+		
+		// Limitando o uso de caracteres
+		System.out.println();
+		String teste5 = String.format("|%.4s|", "|Victor Henrique|");
+		System.out.println(teste5);
+		System.out.println();
+		String teste6 = String.format("|%.4s|", (nome + sobrenome));
+		System.out.println(teste6);
+		
 	}
 
 }
