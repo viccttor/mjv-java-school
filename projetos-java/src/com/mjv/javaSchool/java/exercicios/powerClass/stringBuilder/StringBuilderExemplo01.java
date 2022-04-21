362d72bdd75ff8a4b7f4546a500a6b0b6e576a00
Victor Henrique <vhsdm7@gmail.com>
2022-04-21 03:22:37 -0300

    manipulacao de arquivos

---
+++ projetos-java/src/com/mjv/javaSchool/java/exercicios/powerClass/stringBuilder/StringBuilderExemplo01.java
@@ -0,0 +1,15 @@
+package com.mjv.javaSchool.java.exercicios.powerClass.stringBuilder;
+
+public class StringBuilderExemplo01 {
+
+	public static void main(String[] args) {
+
+		//StringBuilder nomes = new StringBuilder();
+		//nomes.append("Victor ");//.append("Henrique ").append("Silva ").append(" De Morais");
+		
+		System.out.println("Victor");
+		
+		
+	}
+
+}
--- projetos-java/src/com/mjv/javaSchool/java/exercicios/powerClass/stringJoiner/StringJoinerExemplo01.java
+++ projetos-java/src/com/mjv/javaSchool/java/exercicios/powerClass/stringJoiner/StringJoinerExemplo01.java
@@ -1,11 +1,12 @@
-package com.mjv.javaSchool.java.exercicios.stringBuilder;
+package com.mjv.javaSchool.java.exercicios.powerClass.stringJoiner;
 
 import java.util.StringJoiner;
 
-public class StringBuilderExemplo01 {
+public class StringJoinerExemplo01 {
 
 	public static void main(String[] args) {
-		// Usado para construir um sequência de caracteres separados por delimitador.
+
+	// Usado para construir um sequência de caracteres separados por delimitador.
 		
 		StringJoiner teste01 = new StringJoiner(",");
 		teste01.add("Olá");
@@ -15,8 +16,6 @@ public class StringBuilderExemplo01 {
 		System.out.println(teste01);
 		
 		System.out.println("Sai maldição: " + teste01);
-		
-		
 	}
 
 }
