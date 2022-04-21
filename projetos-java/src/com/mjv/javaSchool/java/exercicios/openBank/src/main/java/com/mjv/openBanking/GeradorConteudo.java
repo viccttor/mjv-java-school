0735b962f0a5fda9e48351ed0f6d29a2242800e5
Victor Henrique <vhsdm7@gmail.com>
2022-04-21 00:34:52 -0300

    Criado metodo para criar e escrever arquivo txt

--- projetos-java/src/com/mjv/javaSchool/java/exercicios/openBank/src/main/java/com/mjv/openBanking/GeradorConteudo.java
+++ projetos-java/src/com/mjv/javaSchool/java/exercicios/openBank/src/main/java/com/mjv/openBanking/GeradorConteudo.java
@@ -3,28 +3,30 @@ package com.mjv.openBanking;
 public class GeradorConteudo {
 
 	public String gerar(Movimentacao movimentacao) {
-		
+
 		StringBuilder sb = new StringBuilder();
 		sb.append(movimentacao.getData().toString().replaceAll("-", ""));
-		sb.append(String.format("%014d", Long.valueOf(movimentacao.getCpfCnpj().replaceAll("[\\D]", ""))));
-		String nome = movimentacao.getNomeCliente();
 		
-		if (nome.length() >= 30) 
-			sb.append(nome.substring(0,30));
+		String cpfCnpj = movimentacao.getCpfCnpj().replaceAll("[\\D]", "");
+		Long cpfCnpjLong = Long.valueOf(cpfCnpj);
+		String cpfCnpjFormatado = String.format("%014d", cpfCnpjLong);
+		sb.append(cpfCnpjFormatado);
+		
+		String nome = movimentacao.getNomeCliente();
+		if (nome.length() >= 30)
+			sb.append(nome.substring(0, 30));
 		else
 			sb.append(String.format("%-29s", nome));
-		
+
 		sb.append(String.format("%010d", Long.valueOf(movimentacao.getValor().toString().replaceAll("[\\D]", ""))));
 		sb.append(movimentacao.getTipo().getSigla());
-		
+
 		if (movimentacao.getEstornada())
 			sb.append("1");
 		else
 			sb.append("0");
-				
+
 		return sb.toString();
 	}
-	
-}		//2022041600013521779118RAIMUNDO NONATO LOUREIRO CAST0000127548R0
-		//2022041600013521779118RAIMUNDO NONATO LOUREIRO CAST0000127548R0
 
+} 
