8d35cbeee89526aa78a8d0c84618eb185983004f
Victor Henrique <vhsdm7@gmail.com>
2022-04-21 02:50:52 -0300

    Create StringExemplo01.java

---
+++ projetos-java/src/com/mjv/javaSchool/java/exercicios/powerClass/LangString/StringExemplo01.java
@@ -0,0 +1,42 @@
+package com.mjv.javaSchool.java.exercicios.powerClass.LangString;
+
+public class StringExemplo01 {
+	
+	String nome;
+	
+	// Construtor
+	StringExemplo01 () {
+		
+	}
+	
+	
+	StringExemplo01 (String nome) {
+		this.nome = nome;
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		// Instanciando uma String
+		String ola = new String("Olá Mundo!");
+		System.out.println(ola);
+		System.out.println();
+		
+		// forma mais pratica
+		System.out.println("Olá Mundo");
+		
+		// Concatenação e quebra de linha
+		System.out.println("Victor\n Henrique " + 22);
+		
+		// Chamando Construtor
+		
+		StringExemplo01 t1 = new StringExemplo01();
+		t1.nome = "Victor Henrique";
+		System.out.println(t1.nome.toString());
+	
+		StringExemplo01 t2 = new StringExemplo01("Victor Henrique");
+		System.out.println(t2.nome.toString());
+		
+	}
+
+}
