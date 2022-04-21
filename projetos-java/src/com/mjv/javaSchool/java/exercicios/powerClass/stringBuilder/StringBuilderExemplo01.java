fffcb25bdae3097aa8657a1a213eb8831b36780d
Victor Henrique <vhsdm7@gmail.com>
2022-04-21 03:40:41 -0300

    Update StringBuilderExemplo01.java

--- projetos-java/src/com/mjv/javaSchool/java/exercicios/powerClass/stringBuilder/StringBuilderExemplo01.java
+++ projetos-java/src/com/mjv/javaSchool/java/exercicios/powerClass/stringBuilder/StringBuilderExemplo01.java
@@ -4,11 +4,25 @@ public class StringBuilderExemplo01 {
 
 	public static void main(String[] args) {
 
-		//StringBuilder nomes = new StringBuilder();
-		//nomes.append("Victor ");//.append("Henrique ").append("Silva ").append(" De Morais");
+		StringBuilder nomes = new StringBuilder();
+		nomes.append("Victor ").append("Henrique ").append("Silva ").append(" De Morais");
 		
-		System.out.println("Victor");
+		// Precisa converter para String para poder mostrar todos juntos
+		System.out.println(nomes.toString());
 		
+		// Mostra a quantidade de caracteres
+		System.out.println(nomes.length());
+
+		// Mostra a quantidade de carcater que pode ser armazenado sem adicionar memória
+		System.out.println(nomes.capacity());
+		
+		// Delimita a quantidade de caracteres
+		nomes.setLength(10);
+		System.out.println(nomes.toString());
+		
+		// Seta um caracter pela posicao
+		nomes.setCharAt(1, 'V');;
+		System.out.println(nomes.toString());
 		
 	}
 
