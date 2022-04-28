c6c637fb576fcbb35515fad9399962cfafaacb5b
Victor Henrique <vhsdm7@gmail.com>
2022-04-28 13:45:00 -0300

    update directory and files

--- projetos-java/src/com/mjv/javaSchool/java/exercicios/aguaLuz/meu-exemplo/agualuzNotificacao/case02/src/main/java/com/mjv/javaSchool/notification/service/Mensagem.java
+++ projetos-java/src/com/mjv/javaSchool/java/exercicios/aguaLuz/meu-exemplo/agualuzNotificacao/case02/src/main/java/com/mjv/javaSchool/notification/service/Mensagem.java
@@ -1,26 +1,26 @@
-package com.mjv.aguaLuz.notification.servico;
+package com.mjv.javaSchool.notification.service;
 
-import com.mjv.aguaLuz.notification.model.Cadastro;
-import com.mjv.aguaLuz.notification.model.Contrato;
-import com.mjv.aguaLuz.notification.util.FormatadorUtil;
+import com.mjv.javaSchool.notification.gerador.GeradorCadastro;
+import com.mjv.javaSchool.notification.gerador.GeradorContrato;
+import com.mjv.javaSchool.notification.util.FormatadorUtil;
 
-public class GeradorMensagem {
-	
-	public void gerar (Contrato contrato) {
+public class Mensagem {
+	public void gerarMensagem (GeradorContrato contrato) {
 		StringBuilder sb = new StringBuilder();
-		Cadastro cliente = contrato.getCliente();
+		GeradorCadastro cliente = contrato.getCliente();
 		String cpfFormatado = FormatadorUtil.formatarCpf(cliente.getCpf());
 		sb.append("Senhor(a) "+ cliente.getNome() + " cpf de número "+ cpfFormatado);
-		sb.append(String.format(", Informamos que conforme contrato com protocolo de número %s está agendado para a data\\hora", contrato.getNumeroProtocolo()));
+		sb.append(String.format(", Informamos que conforme contrato com protocolo de número %s está agendado para a data\\hora", contrato.getNumProtocolo()));
 		String dataFormatada = FormatadorUtil.formatarDataHora(contrato.getDataHora());
 		String valorFormatadoBr = FormatadorUtil.formatarValorBr(contrato.getValor());
 		sb.append(String.format(" %sh a instalação do serviço de %s com taxa de valor R$ %s em sua residência localizada no endereço abaixo:", dataFormatada,contrato.getServico(),valorFormatadoBr));
-		sb.append(String.format("\n\n° Lagradouro: %s,%s", cliente.getEndereco().getLagradouro(),cliente.getEndereco().getNumero()));
+		sb.append(String.format("\n\n° Lagradouro: %s,%s", cliente.getEndereco().getLagradouro(),cliente.getEndereco().getNumResidencia()));
 		sb.append(String.format("\n° Complemento: %s", cliente.getEndereco().getComplemento()));
 		sb.append(String.format("\n° Bairro: %s", cliente.getEndereco().getBairro()));
 		sb.append(String.format("\n° Cidade: %s / %s", cliente.getEndereco().getCidade(),cliente.getEndereco().getEstado()));
 		String cepFormatado = FormatadorUtil.formatarCep(cliente.getEndereco().getCep());
 		sb.append(String.format("\n° Cep: %s", cepFormatado));
 		System.out.println(sb.toString());
+		
 	}
 }
